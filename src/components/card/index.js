0d6e45fcb3c2e64656895aa7b9bb8eef11d01720
Erick Cardoso <cyber666king@gmail.com>
2021-09-27 11:57:16 -0300

    implementação do css

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -4,12 +4,47 @@ const Card = ({ titulo, texto }) => {
     return (
         <form className="form_css">
             <fieldset className= "fieldset_css">
-                <legend className="titulo_css">Formulário de cadastro</legend>
+                <legend className="titulo_css">FORMULÁRIO DE CADASTRO</legend>
                 <input className="input_css" type='text' placeholder='Nome:'></input>
                 <input className="input_css" type='email' placeholder='Email:'></input>
                 <input className="input_css" type='password' placeholder='Senha:'></input>
                 <button className="send_button_css" type='submit'>ENVIAR</button>
             </fieldset>
+            <div className="tabela">
+                <table>
+                    <tr>
+                        <th className="titulo">NOME</th>
+                        <th >EMAIL</th>
+                        <th>SENHA</th>
+                        <th>AÇÕES</th>
+                    </tr>
+                    <tr>
+                        <td>Fulana da Silva</td>
+                        <td>fulana@gmail.com</td>
+                        <td>abc123</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Fulana da Silva</td>
+                        <td>fulana@gmail.com</td>
+                        <td>abc123</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Fulana da Silva</td>
+                        <td>fulana@gmail.com</td>
+                        <td>abc123</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <td>Fulana da Silva</td>
+                        <td>fulana@gmail.com</td>
+                        <td>abc123</td>
+                        <td></td>
+                    </tr>
+                   
+                </table>
+            </div>
         </form>
     );
 }
